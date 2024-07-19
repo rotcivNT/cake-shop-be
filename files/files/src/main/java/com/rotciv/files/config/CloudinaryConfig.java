3503c573a8ce92ffac4aaeee1d1c2342656bc55d
rotciv <ngocthang.devweb@gmail.com>
2024-07-20 00:00:48 +0700

    a

--- files/files/src/main/java/com/rotciv/files/config/CloudinaryConfig.java
+++ files/files/src/main/java/com/rotciv/files/config/CloudinaryConfig.java
@@ -8,12 +8,21 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudinaryConfig {
-
+    @Value("${cloudinary.cloud_name}")
+    private String cloudName;
+    @Value("${cloudinary.api_key}")
+    private String apiKey;
+    @Value("${cloudinary.api_secret}")
+    private String apiSecret;
     @Bean
     public Cloudinary cloudinary() {
+//        return new Cloudinary(ObjectUtils.asMap(
+//                "cloud_name", "dwt8nm72d",
+//                "api_key", "118398793585282",
+//                "api_secret", "VPqOIM8CNQEVMAj6P_vCw5jl8sQ"));
         return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "dwt8nm72d",
-                "api_key", "118398793585282",
-                "api_secret", "VPqOIM8CNQEVMAj6P_vCw5jl8sQ"));
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret));
     }
 }
